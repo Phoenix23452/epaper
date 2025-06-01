a83b08fcd76f1bac1267ab68eae93eb6f5ea561b
Phoenix <1phoenix23452@gmail.com>
2025-06-01 18:45:44 +0500

    Fix Users managment

--- src/app/(admin)/v2/admin/SessionWrapper.tsx
+++ src/app/(admin)/v2/admin/SessionWrapper.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+"use client";
 
-export default function SessionWrapper() {
-  return <div></div>;
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+export default function SessionWrapper({ children }: { children: ReactNode }) {
+  return <SessionProvider>{children}</SessionProvider>;
 }
--- src/app/(admin)/v2/admin/layout.tsx
+++ src/app/(admin)/v2/admin/layout.tsx
@@ -1,16 +1,19 @@
 import { AdminSidebar } from "@/components/admin/AdminSidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import React, { ReactNode } from "react";
+import SessionWrapper from "./SessionWrapper";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   return (
-    <SidebarProvider>
-      <AdminSidebar />
+    <SessionWrapper>
+      <SidebarProvider>
+        <AdminSidebar />
 
-      <main className="w-full">
-        <SidebarTrigger />
-        <div className="px-8 ">{children}</div>
-      </main>
-    </SidebarProvider>
+        <main className="w-full">
+          <SidebarTrigger />
+          <div className="px-8 ">{children}</div>
+        </main>
+      </SidebarProvider>
+    </SessionWrapper>
   );
 }
